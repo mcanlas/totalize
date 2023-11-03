ba1838a1862c4c70cc079d256e4f52debdc542ff
Mark Canlas <github@htmlism.com>
2023-11-03 13:55:29 -0400

    bootstrap

---
+++ project/ProjectPlugin.scala
@@ -0,0 +1,58 @@
+import sbt.Keys.*
+import sbt.*
+
+/**
+  * Automatically enriches projects with the following settings (despite the word "override").
+  */
+object ProjectPlugin extends AutoPlugin {
+
+  /**
+    * Defines what members will be imported to the `build.sbt` scope.
+    */
+  val autoImport = ThingsToAutoImport
+
+  /**
+    * Thus plug-in will automatically be enabled; it has no requirements.
+    */
+  override def trigger: PluginTrigger = AllRequirements
+
+  object ThingsToAutoImport {
+    private def jarName(s: String) =
+      "temporal-diagrams-" + s
+
+    def module(s: String): Project =
+      Project(s, file(jarName(s)))
+        .settings(name := jarName(s))
+
+    implicit class ProjectOps(p: Project) {
+      val http4sVersion =
+        "0.23.23"
+
+      def withCats: Project =
+        p
+          .settings(libraryDependencies += "org.typelevel" %% "cats-core" % "2.10.0")
+
+      def withHttpServer: Project =
+        p
+          .settings(
+            libraryDependencies ++= Seq(
+              "org.http4s" %% "http4s-dsl"          % http4sVersion,
+              "org.http4s" %% "http4s-blaze-server" % "0.23.15"
+            )
+          )
+
+      def withTesting: Project = {
+        val weaverVersion =
+          "0.8.3"
+
+        p.settings(
+          testFrameworks += new TestFramework("weaver.framework.CatsEffect"),
+          libraryDependencies ++= Seq(
+            "com.disneystreaming" %% "weaver-cats"       % weaverVersion % Test,
+            "com.disneystreaming" %% "weaver-scalacheck" % weaverVersion % Test
+          )
+        )
+      }
+    }
+  }
+}
---
+++ project/Scala3Plugin.scala
@@ -0,0 +1,21 @@
+import sbt.Keys.*
+import sbt.*
+
+/**
+  * Automatically enriches projects with the following settings (despite the word "override").
+  */
+object Scala3Plugin extends AutoPlugin {
+
+  /**
+    * Thus plug-in will automatically be enabled; it has no requirements.
+    */
+  override def trigger: PluginTrigger = AllRequirements
+
+  override val buildSettings: Seq[Setting[?]] = Seq(
+    scalaVersion := "3.3.1"
+  )
+
+  override val projectSettings: Seq[Setting[?]] = Seq(
+    scalacOptions ++= Seq("-indent", "-rewrite")
+  )
+}
