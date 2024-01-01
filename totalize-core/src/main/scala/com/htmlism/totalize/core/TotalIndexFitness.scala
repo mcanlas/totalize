1f6443fb1fe47a3acadb6e9037374831dca51783
Mark Canlas <github@htmlism.com>
2023-12-31 22:04:32 -0500

    add defensive code

--- totalize-core/src/main/scala/com/htmlism/totalize/core/TotalIndexFitness.scala
+++ totalize-core/src/main/scala/com/htmlism/totalize/core/TotalIndexFitness.scala
@@ -5,11 +5,12 @@ object TotalIndexFitness:
     val order =
       index.toOrder
 
+    // TODO the constructed orderings are actually partial and fallible, a la partial function
     prefs
       .xs
       .map:
         case (Pair(x, y), pref) =>
-          if order.lt(x, y) && pref == BinaryPreference.First then 1
-          else if order.lt(y, x) && pref == BinaryPreference.Second then 1
+          if util.Try(order.lt(x, y)).getOrElse(false) && pref == BinaryPreference.First then 1
+          else if util.Try(order.lt(y, x)).getOrElse(false) && pref == BinaryPreference.Second then 1
           else 0
       .sum
