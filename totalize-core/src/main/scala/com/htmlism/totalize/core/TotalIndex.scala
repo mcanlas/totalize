fd01d6c1272c911f8e0cd40ecd80c4d8f095d762
Mark Canlas <github@htmlism.com>
2023-12-31 21:42:53 -0500

    add note

--- totalize-core/src/main/scala/com/htmlism/totalize/core/TotalIndex.scala
+++ totalize-core/src/main/scala/com/htmlism/totalize/core/TotalIndex.scala
@@ -2,6 +2,11 @@ package com.htmlism.totalize.core
 
 import cats.*
 
+/**
+  * This index is constructed from an input population of `A`.
+  *
+  * But it is still possible that queries to its order may not have been in the input population during construction.
+  */
 case class TotalIndex[A: Eq](xs: List[A]):
   private lazy val _map =
     xs.iterator.zipWithIndex.toMap
@@ -9,6 +14,7 @@ case class TotalIndex[A: Eq](xs: List[A]):
   private lazy val _vector =
     xs.toVector
 
+  // where do unexpected elements get sorted?
   def getIndex(x: A): Int =
     _map(x)
 
