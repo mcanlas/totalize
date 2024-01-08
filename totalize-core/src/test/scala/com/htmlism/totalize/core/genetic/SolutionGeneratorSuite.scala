7abd02a374875b200e6fa3a2d50eadf33155c00e
Mark Canlas <github@htmlism.com>
2024-01-08 15:44:25 -0500

    use arbitrary size

--- totalize-core/src/test/scala/com/htmlism/totalize/core/genetic/SolutionGeneratorSuite.scala
+++ totalize-core/src/test/scala/com/htmlism/totalize/core/genetic/SolutionGeneratorSuite.scala
@@ -1,24 +1,30 @@
 package com.htmlism.totalize.core.genetic
 
 import cats.effect.*
+import org.scalacheck.Gen
 import weaver.SimpleIOSuite
 import weaver.scalacheck.Checkers
 
 object SolutionGeneratorSuite extends SimpleIOSuite with Checkers:
+  val genSize =
+    Gen.choose(0, 100)
+
   test("A generator can generate a collection of N, in sequence"):
-    for
-      rng <- std.Random.scalaUtilRandom[IO]
-      xs  <- SolutionGenerator.sync[IO](rng).generate(3)
-    yield expect.eql(
-      3,
-      xs.length
-    )
+    forall(genSize): n =>
+      for
+        rng <- std.Random.scalaUtilRandom[IO]
+        xs  <- SolutionGenerator.sync[IO](rng).generate(n)
+      yield expect.eql(
+        n,
+        xs.length
+      )
 
   test("A generator can generate a collection of N, in parallel"):
-    for
-      rng <- std.Random.scalaUtilRandom[IO]
-      xs  <- SolutionGenerator.parallel[IO](rng).generate(3)
-    yield expect.eql(
-      3,
-      xs.length
-    )
+    forall(genSize): n =>
+      for
+        rng <- std.Random.scalaUtilRandom[IO]
+        xs  <- SolutionGenerator.parallel[IO](rng).generate(n)
+      yield expect.eql(
+        n,
+        xs.length
+      )
