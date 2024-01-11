836798ea80f251d2e1ad3438d3e3b9ed759ed6d0
Mark Canlas <github@htmlism.com>
2024-01-11 07:52:48 -0500

    solution decoder stubs

---
+++ totalize-core/src/main/scala/com/htmlism/totalize/core/genetic/SolutionDecoder.scala
@@ -0,0 +1,17 @@
+package com.htmlism.totalize.core.genetic
+
+import cats.syntax.all.*
+
+/**
+  * @tparam A
+  *   The encoded type
+  * @tparam B
+  *   The domain type
+  */
+trait SolutionDecoder[A, B]:
+  def decode(x: A): Either[String, B]
+
+class TotalIndexDecoder(ids: List[String]) extends SolutionDecoder[Array[Int], List[String]]:
+  def decode(xs: Array[Int]): Either[String, List[String]] =
+    if xs.length == ids.length then Nil.asRight
+    else s"Input length ${xs.length} does not match population length ${ids.length}".asLeft
---
+++ totalize-core/src/test/scala/com/htmlism/totalize/core/genetic/SolutionDecoderSuite.scala
@@ -0,0 +1,6 @@
+package com.htmlism.totalize.core.genetic
+
+import weaver.SimpleIOSuite
+import weaver.scalacheck.Checkers
+
+object SolutionDecoderSuite extends SimpleIOSuite with Checkers
