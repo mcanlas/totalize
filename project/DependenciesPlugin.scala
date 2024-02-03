38de59bea45268c706be9c19779555ed11a4b498
Mark Canlas <github@htmlism.com>
2024-02-02 19:18:34 -0500

    weaver 0.8.4

--- project/DependenciesPlugin.scala
+++ project/DependenciesPlugin.scala
@@ -29,7 +29,7 @@ object DependenciesPlugin extends AutoPlugin {
 
       def withTesting: Project = {
         val weaverVersion =
-          "0.8.3"
+          "0.8.4"
 
         p.settings(
           testFrameworks += new TestFramework("weaver.framework.CatsEffect"),
