2606088cf778c3850c3545fb4508883c746dee47
Mark Canlas <github@htmlism.com>
2024-04-10 01:42:09 -0400

    fix unwired test

--- totalize-core/src/test/scala/com/htmlism/totalize/core/PairSuite.scala
+++ totalize-core/src/test/scala/com/htmlism/totalize/core/PairSuite.scala
@@ -8,10 +8,10 @@ object PairSuite extends FunSuite:
     whenSuccess(Pair.from(1, 2)): xs =>
       expect.same(Pair(1, 2), xs)
 
-  test("Constructing a pair fails if the arguments are the same")
-  matches(Pair.from(1, 1)):
-    case Left(_) =>
-      success
+  test("Constructing a pair fails if the arguments are the same"):
+    matches(Pair.from(1, 1)):
+      case Left(_) =>
+        success
 
   test("Pairs are equivalent regardless of their safe construction order"):
     whenSuccess(
