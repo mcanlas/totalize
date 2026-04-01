62d583686b626009559acf92f85d56f11688eec2
Mark Canlas <github@htmlism.com>
2026-03-31 20:35:01 -0400

    drop unused

--- totalize-core/src/main/scala/com/htmlism/totalize/core/TotalIndexGenerator.scala
+++ totalize-core/src/main/scala/com/htmlism/totalize/core/TotalIndexGenerator.scala
@@ -6,11 +6,11 @@ import cats.*
 import cats.syntax.all.*
 
 object TotalIndexGenerator:
-  def generate[A: Eq](rng: Random, xs: List[A]): TotalIndex[A] =
+  def generate[A](rng: Random, xs: List[A]): TotalIndex[A] =
     TotalIndex:
       rng.shuffle(xs)
 
-  def generateN[F[_]: Applicative, A: Eq](shuffle: List[A] => F[List[A]], xs: List[A], n: Int): F[List[TotalIndex[A]]] =
+  def generateN[F[_]: Applicative, A](shuffle: List[A] => F[List[A]], xs: List[A], n: Int): F[List[TotalIndex[A]]] =
     (0 until n)
       .toList
       .traverse(_ => shuffle(xs).map(TotalIndex(_)))
